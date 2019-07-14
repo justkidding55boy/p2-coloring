9ca728489fd6d095bbc000a49e4acab59f7f2feb
eijikudo <eijikudo@berkeley.edu>
2019-07-14 19:03:48 -0400

    Upload the add-image function

---
+++ coloring/static/coloring/javascript/index.js
@@ -0,0 +1,23 @@
+  $(document).ready(function() {
+    $('.color-main').hover(
+      function() {
+        $('#color-palette').css('display', 'flex');
+      },
+      function() {
+
+      });
+    
+        $('#color-palette').hover(
+      function() {
+      },
+      function() {
+        $('#color-palette').css('display', 'none');
+      });
+
+  });
+    
+
+
+  
+  
+	
